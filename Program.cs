1895f24ccb8aadb24917cf143f9b38360a5921d2
Nic Gerhardt <NicolasGerhardt@quickenloans.com>
2020-02-04 16:25:53 -0500

    Refactored to remove unnessisary loops and methods

--- Program.cs
+++ Program.cs
@@ -20,7 +20,7 @@ namespace GC_Lab_4._1
                     rollCount++;
                     int[] DiceResults = RollDice(numberOFDice, sides);
                     DisplayDiceResults(DiceResults, rollCount);
-                    DiceResultsAnalsys(rollCount, sides, DiceResults);
+                    DiceResultsAnalsys(sides, DiceResults);
 
                 } while (PromptYesNo("Roll Again? (y/n) "));
             } while (PromptYesNo("Do you want different dice? (y/n) "));
@@ -29,35 +29,36 @@ namespace GC_Lab_4._1
 
         }
 
-        private static void DiceResultsAnalsys(int rollCount, int sides, int[] diceResults)
+        private static void DiceResultsAnalsys(int sides, int[] diceResults)
         {
+            int[] countOfEachSideRolled = new int[sides + 1]; //made bigger to accomidate 0 indexing translation
+            int sum = DiceSum(diceResults);
+            foreach (int die in diceResults)
+            {
+                countOfEachSideRolled[die]++;
+            }
+
             if (diceResults.Length == 2 && sides == 6)
             {
-                if (IsSnakeEyes(diceResults)) WriteColor("SnakeEyes\n", ConsoleColor.Green);
-                if (IsAceDuce(diceResults)) Console.WriteLine("Ace Duce");
-                if (IsBoxCars(diceResults)) Console.WriteLine("BoxCars");
-                if (IsCrapsWin(diceResults)) WriteColor("WIN!!!\n", ConsoleColor.Blue);
-                if (IsCrapsLoss(diceResults)) WriteColor("Craps!!\n", ConsoleColor.Red);
+                if (countOfEachSideRolled[1] == 2) WriteColor("Snake Eyes\n", ConsoleColor.Green);
+                if (countOfEachSideRolled[1] == 1 && countOfEachSideRolled[2] == 1) WriteColor("Ace Duce\n", ConsoleColor.Red);
+                if (countOfEachSideRolled[6] == 2) WriteColor("Boxcars\n", ConsoleColor.Red);
+                if (sum == 7 || sum == 11) WriteColor("WIN!!!\n", ConsoleColor.Cyan);
+                if (sum == 2 || sum == 3 || sum == 12) WriteColor("Craps!!\n", ConsoleColor.Red);
             }
 
             if (diceResults.Length == 5 && sides == 6) 
             {
-                if (IsYahtzee(diceResults)) DisplayBanner("YAHTZEE!!!");
+                if (IsYahtzee(countOfEachSideRolled)) DisplayBanner("YAHTZEE!!!");
             }
 
             Console.WriteLine();
             Console.WriteLine($"You rolled a total of {DiceSum(diceResults)}.");
         }
 
-        private static bool IsYahtzee(int[] diceResults)
+        private static bool IsYahtzee(int[] countOfEachSideRolled)
         {
-            int[] numOfEachNumberRolled = new int[6];
-
-            foreach (int dice in diceResults)
-            {
-                numOfEachNumberRolled[dice - 1]++;
-            }
-            foreach (int result in numOfEachNumberRolled)
+            foreach (int result in countOfEachSideRolled)
             {
                 if (result == 5) return true;
             }
@@ -65,11 +66,6 @@ namespace GC_Lab_4._1
             return false;
         }
 
-        private static bool IsCrapsLoss(int[] diceResults)
-        {
-            int sum = DiceSum(diceResults);
-            return (sum == 2 || sum == 3 || sum == 12);
-        }
 
         private static int DiceSum(int[] diceResults)
         {
@@ -79,49 +75,6 @@ namespace GC_Lab_4._1
                 sum += dice;
             }
             return sum;
-        }
-
-        private static bool IsCrapsWin(int[] diceResults)
-        {
-            int sum = DiceSum(diceResults);
-            return (sum == 7 || sum == 11);
-        }
-
-        private static bool IsBoxCars(int[] diceResults)
-        {
-            int sixesCount = 0;
-            foreach (int dice in diceResults)
-            {
-                if (dice == 6) sixesCount++;
-            }
-
-            return sixesCount == 2;
-        }
-
-        private static bool IsAceDuce(int[] diceResults)
-        {
-            bool hasOne = false;
-            bool hasTwo = false;
-
-            foreach (int dice in diceResults)
-            {
-                if (dice == 1) hasOne = true;
-                if (dice == 2) hasTwo = true;
-            }
-
-            return (hasOne && hasTwo);
-        }
-
-        private static bool IsSnakeEyes(int[] diceResults)
-        {
-            if (diceResults.Length == 2)
-            {
-                if (diceResults[0] == 1 && diceResults[1] == 1)
-                {
-                    return true;
-                }
-            }
-            return false;
         }
 
         private static void DisplayBanner(string bannerMessage)
@@ -138,12 +91,7 @@ namespace GC_Lab_4._1
             Console.WriteLine();
         }
 
-        /// <summary>
-        /// Yes Responce retruns true, no responce returns false. 
-        /// Otherwise it keeps looping asking for yes or no.
-        /// </summary>
-        /// <param name="prompt"></param>
-        /// <returns></returns>
+
         private static bool PromptYesNo(string prompt)
         {
             do
@@ -178,12 +126,7 @@ namespace GC_Lab_4._1
             Console.WriteLine();
         }
 
-        /// <summary>
-        /// Rolls a number of dice with the provided number or sides.
-        /// </summary>
-        /// <param name="numberOfDice"></param>
-        /// <param name="sides"></param>
-        /// <returns></returns>
+
         private static int[] RollDice(int num, int sides)
         {
             int[] dice = new int[num];
